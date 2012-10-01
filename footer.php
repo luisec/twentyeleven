ef0bf1bc55932e5dd34cf546b724a46222563653
Luis Calderon <luisec@gmail.com>
2012-10-01 18:32:30 -0500

    Pagina base de blog
    Pagina basse con menu dinamico.

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+    <div id="footer">
+    	<ul id="categorias">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Privacidad</a></li>
+            <li><a href="#">Condiciones</a></li>
+        </ul>
+    </div>
+    
+</div>
+
+</body>
+</html>
---
+++ main-template.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * Template Name: Main Template
+ * Description: A Page Template that adds a sidebar to pages
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+				<?php //while ( have_posts() ) : the_post(); ?>
+					<?php //get_template_part( 'content', 'page' ); ?>
+					<?php //comments_template( '', true ); ?>
+				<?php //endwhile; // end of the loop. ?>
+
+<div id="bgd">
+    <div id="content">
+    	<div id="articulos">
+        	<div id="articulo_top">Ultimas Noticias</div>
+            <div id="articulo_cont">
+            	<div class="articulo_articulo">
+                	<div class="articulo_img">Imagen</div>
+                    <div class="articulo_cont">
+                    	<div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="articulo_titulo"><a href="#">Titulo del Artículo</a></div>
+                        <div class="articulo_resena">
+                        	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 			Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="articulo_articulo">
+                	<div class="articulo_img">Imagen</div>
+                    <div class="articulo_cont">
+                    	<div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="articulo_titulo"><a href="#">Titulo de la Galería</a></div>
+                        <div class="articulo_resena">
+                        	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 			Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                        </div>
+                    </div>
+                </div>
+                	
+                <div class="articulo_articulo">
+                	<div class="articulo_img">Imagen</div>
+                    <div class="articulo_cont">
+                    	<div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="articulo_titulo"><a href="#">Titulo de la Cita</a></div>
+                        <div class="articulo_resena">
+                        	<p>C'est en faisant n'importe quoi qu'on devient n'importe qui</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="articulo_articulo">
+                	<div class="articulo_img">Imagen</div>
+                    <div class="articulo_cont">
+                    	<div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="articulo_titulo"><a href="#">Titulo del Artículo</a></div>
+                        <div class="articulo_resena">
+                        	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 			Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="articulo_articulo">
+                	<div class="articulo_img">Imagen</div>
+                    <div class="articulo_cont">
+                    	<div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="articulo_titulo"><a href="#">Titulo de la Galería</a></div>
+                        <div class="articulo_resena">
+                        	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 			Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                        </div>
+                    </div>
+                </div>
+                	
+                <div class="articulo_articulo">
+                	<div class="articulo_img">Imagen</div>
+                    <div class="articulo_cont">
+                    	<div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="articulo_titulo"><a href="#">Titulo de la Cita</a></div>
+                        <div class="articulo_resena">
+                        	<p>C'est en faisant n'importe quoi qu'on devient n'importe qui</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="articulo_articulo"> paginacion </div>
+                
+                <br />
+            </div>
+        </div>
+        <div id="menu_der">
+        	<div id="destacados">
+            	<div class="menu_der_titulo">Noticias Destacadas</div>
+				<div class="menu_der_contenedor">
+                	<div class="menu_der_subtitulo"><a href="#">Nombre de la Noticia</a></div>
+                    <div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                </div>
+                
+                <div class="menu_der_contenedor">
+                	<div class="menu_der_subtitulo"><a href="#">Nombre de la Noticia</a></div>
+                    <div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                </div>
+                
+                <div class="menu_der_contenedor">
+                	<div class="menu_der_subtitulo"><a href="#">Nombre de la Noticia</a></div>
+                    <div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                </div>
+                
+                <div class="menu_der_contenedor">
+                	<div class="menu_der_subtitulo"><a href="#">Nombre de la Noticia</a></div>
+                    <div class="articulo_datos">
+                        	<table>
+                            	<tr>
+                                	<td>Autor</td>
+                                    <td>Fecha de Publicación</td>
+                                </tr>
+                            </table>
+                        </div>
+                </div>
+            </div>
+            <div id="editores">
+            	<div class="menu_der_titulo">Editores</div>
+                <br />
+                <div class="menu_der_contenedor">
+                	<div class="foto_editor"></div>
+                    <div class="editores_datos">
+	                    <div class="editor_subtitulo"><a href="#">Rol</a></div>
+                        <div class="editor_subtitulo">Nombre del Editor</div>
+                    </div>
+                </div>
+                
+                <div class="menu_der_contenedor">
+                	<div class="foto_editor"></div>
+                    <div class="editores_datos">
+	                    <div class="editor_subtitulo"><a href="#">Rol</a></div>
+                        <div class="editor_subtitulo">Nombre del Editor</div>
+                    </div>
+                </div>
+                
+                <div class="menu_der_contenedor">
+                	<div class="foto_editor"></div>
+                    <div class="editores_datos">
+	                    <div class="editor_subtitulo"><a href="#">Rol</a></div>
+                        <div class="editor_subtitulo">Nombre del Editor</div>
+                    </div>
+                </div>
+                
+                <div class="menu_der_contenedor">
+                	<div class="foto_editor"></div>
+                    <div class="editores_datos">
+	                    <div class="editor_subtitulo"><a href="#">Rol</a></div>
+                        <div class="editor_subtitulo">Nombre del Editor</div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="editores">
+            	<table id="contacto">
+                	<tr>
+                    	<td><img src="Content/Icons/30x30Facebook.png" alt="facebook" /></td>
+                        <td><img src="Content/Icons/30x30Twitter.png" alt="twitter" /></td>
+                        <td><img src="Content/Icons/30x30Pinterest.png" alt="pinterest" /></td>
+                        <td><img src="Content/Icons/30x30RSS.png" alt="RSS" /></td>
+                    </tr>
+                </table>
+            </div>
+            
+        </div>
+    </div>
+    
+
+
+
+<?php get_footer(); ?>
